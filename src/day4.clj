67b32319fe87ff067a0a8c4384e493c949f1f938
Michiel Borkent <michielborkent@gmail.com>
2017-12-04 09:58:58 +0100

    Day 4 cleanup

--- src/day4.clj
+++ src/day4.clj
@@ -1,25 +1,28 @@
 (ns day4
   (:require [clojure.java.io :as io]
-            [clojure.string :as str]
             [clojure.math.combinatorics :as combo]
+            [clojure.string :as str]
             [util :refer [resource-reducible]]))
 
-(defn valid-passphrase? [phrase]
+(defn valid-passphrase?
+  [phrase]
   (= (count phrase)
      (count (set phrase))))
 
-(defn part-1 []
+(defn part-1
+  []
   (transduce
    (comp
     (map #(str/split % #"\s"))
-    (keep #(when (valid-passphrase? %)
-             1)))
+    (filter valid-passphrase?)
+    (map (constantly 1)))
    +
    (resource-reducible "day4.txt")))
 
-(defn anagrams [word]
+(defn anagrams
+  [word]
   (map #(apply str %)
-       (combo/permutations word)))
+       (rest (combo/permutations word))))
 
 (defn anagram-in-words?
   [words word]
@@ -34,13 +37,14 @@
                  (disj words %) %)
                words))))
 
-(defn part-2 []
+(defn part-2
+  []
   (transduce
    (comp
     (map #(str/split % #"\s"))
-    (filter #(valid-passphrase? %))
-    (keep #(when (valid-anagram-passphrase? %)
-             1)))
+    (filter valid-passphrase?)
+    (filter valid-anagram-passphrase?)
+    (map (constantly 1)))
    +
    (resource-reducible "day4.txt")))
 
@@ -48,5 +52,5 @@
 
 (comment
   (part-1)
-  (part-2)
+  (time (part-2))
   )
