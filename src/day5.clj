33390872e94f6e71158d228030f870f60510ce51
Michiel Borkent <michielborkent@gmail.com>
2017-12-05 17:49:46 +0100

    Day 5: 74ms

--- src/day5.clj
+++ src/day5.clj
@@ -83,26 +83,26 @@
                (resource-reducible "day5-mfikes.txt"))
          (into-array Integer/TYPE))
         length ^int (alength maze)]
-    (loop [maze maze
-           cur-pos 0
+    (loop [cur-pos 0
            steps 0]
       (if (< cur-pos length)
         (let [cur-val (aget maze cur-pos)]
           (if (zero? cur-val)
-            (recur (doto maze
-                     (aset
-                      cur-pos
-                      2 #_(+ cur-val 2)))
-                   (inc cur-pos)
-                   (+ steps 2))
-            (recur (doto maze
-                     (aset
-                      cur-pos
-                      (if (>= cur-val 3)
-                        (dec cur-val)
-                        (inc cur-val))))
-                   (+ cur-pos cur-val)
-                   (inc steps))))
+            (do
+              (doto maze
+                (aset cur-pos 2))
+              (recur
+               (inc cur-pos)
+               (+ steps 2)))
+            (do (doto maze
+                  (aset
+                   cur-pos
+                   (if (>= cur-val 3)
+                     (dec cur-val)
+                     (inc cur-val))))
+                (recur
+                 (+ cur-pos cur-val)
+                 (inc steps)))))
         steps))))
 
 
@@ -110,7 +110,7 @@
   (time (part-1))
   (time (part-2)) ;; 5.6 s, not fast enough for me
   (time (part-2-faster)) ;; 3.8 s, still sucks
-  
+
   (quick-bench (part-2-array)) ;; 84 ms
-  (quick-bench (part-2-array-faster?)) ;; 78 ms
+  (quick-bench (part-2-array-faster?)) ;; 74 ms
   )
