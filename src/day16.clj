4f45f1ea2b953b5aead47d61a587bc0e4ba0c63d
Michiel Borkent <michielborkent@gmail.com>
2018-01-17 20:45:23 +0100

    Day 16: added clojure.spec parser

--- src/day16.clj
+++ src/day16.clj
@@ -2,6 +2,7 @@
   (:require
    [blancas.kern.core :as k]
    [clojure.edn :as edn]
+   [clojure.spec.alpha :as s]
    [clojure.string :as str]
    [criterium.core :refer [quick-bench]]
    [instaparse.core :as insta]
@@ -14,6 +15,8 @@
   []
   (read-first "day16.txt"))
 
+;;;; Instaparse
+
 (def parse
   (insta/parser
    "<INPUT>       = INSTRUCTION (<','> INSTRUCTION)*
@@ -24,7 +27,7 @@
     POSITION      = #'\\d\\d?'
     PROGRAM       = #'[a-p]'"))
 
-(defn parse-data
+(defn insta-parser
   [d]
   (->>
    d
@@ -61,9 +64,9 @@
 (defn eval-expr
   [progs [tag & args]]
   (let [f (dispatch tag)]
-    (apply f 
-     progs
-     args)))
+    (apply f
+           progs
+           args)))
 
 (defn solve
   [progs parsed]
@@ -72,12 +75,12 @@
           parsed))
 
 (defn part-1
-  ([] (part-1 programs (parse-data (data))))
+  ([] (part-1 programs (insta-parser (data))))
   ([progs parsed]
    (apply str (solve progs parsed))))
 
 (defn part-2
-  ([] (part-2 programs (parse-data (data))))
+  ([] (part-2 programs (insta-parser (data))))
   ([progs parsed]
    (let [iters (iterate #(solve % parsed) progs)
          n-repeat (inc
@@ -97,7 +100,7 @@
   [s]
   (str/split s #"/"))
 
-(defn parse-data2
+(defn handwritten-parser
   [d]
   (map
    (fn [expr]
@@ -141,9 +144,96 @@
 (def parse-input
   (k/sep-by1 (k/sym* \,) parse-instruction))
 
-(defn parse-data3
+(defn kern-parser
   [d]
   (:value (k/parse-data parse-input d)))
+
+;;;; clojure.spec parser
+
+(s/def ::program
+  #(and (>= (int %) (int \a))
+        (<= (int %) (int \p))))
+
+(comment
+  (s/valid? ::program \a) ;; true
+  (s/valid? ::program \z) ;; false
+  )
+
+(s/def ::position
+  (s/&
+   (s/+ #(Character/isDigit ^Character %))
+   (s/conformer
+    (fn [parsed]
+      (Integer/parseInt
+       (apply str parsed))))))
+
+(comment
+  (s/conform ::position (seq "123")) ;; 123
+  (s/conform ::position "z") ;; :clojure.spec.alpha/invalid
+  )
+
+(s/def ::spin
+  (s/and
+   (s/cat :first #{\s}
+          :pos ::position)
+   (s/conformer (juxt :pos))))
+
+(comment
+  (s/conform ::spin (seq "s10")) ;; [10]
+  )
+
+(s/def ::partner
+  (s/and
+   (s/cat :first #{\p}
+          :prog1 ::program
+          :slash #{\/}
+          :prog2 ::program)
+   (s/conformer (juxt :prog1 :prog2))))
+
+(comment
+  (s/conform ::partner (seq "pa/g")) ;; [\a \g]
+  )
+
+(s/def ::exchange
+  (s/and
+   (s/cat :first #{\x}
+          :pos1 ::position
+          :slash #{\/}
+          :pos2 ::position)
+   (s/conformer (juxt :pos1 :pos2))))
+
+(comment
+  (s/conform ::exchange (seq "x11/2")) ;; [11 2]
+  )
+
+(s/def ::instruction
+  (s/and
+   (s/conformer seq)
+   (s/or :SPIN ::spin
+         :EXCHANGE ::exchange
+         :PARTNER ::partner)
+   (s/conformer (comp vec flatten))))
+
+(comment
+  (s/conform ::instruction "s10") ;; [:SPIN 10]
+  (s/conform ::instruction "x11/2") ;; [:EXCHANGE 11 2]
+  (s/conform ::instruction "pa/g") ;; [:PARTNER \a \g]
+  )
+
+(s/def ::instructions
+  (s/and (s/conformer #(str/split % #","))
+         (s/cat :instructions
+                (s/* ::instruction))
+         (s/conformer (fn [parsed]
+                        (:instructions parsed)))))
+
+(comment
+  (s/conform ::instructions "s10,x11/2,pl/g") ;; [[:SPIN 10] [:EXCHANGE 11 2] [:PARTNER \l \g]]
+  )
+
+(defn spec-parser
+  [input]
+  (s/conform ::instructions input))
 
 ;;;; Scratch
 
@@ -151,10 +241,15 @@
   (set! *print-length* 20)
   (set! *warn-on-reflection* true)
   (set! *unchecked-math* :warn-on-boxed)
-  (quick-bench (def parsed1 (doall (parse-data (data)))))  ;; ~582ms
-  (quick-bench (def parsed2 (doall (parse-data2 (data))))) ;; ~7.8ms
-  (quick-bench (def parsed3 (parse-data3 (data))))         ;; ~113ms
-  (= parsed1 parsed2 parsed3) ;; true
-  (quick-bench (part-1 programs parsed1)) ;; ~7ms,   "olgejankfhbmpidc"
-  (quick-bench (part-2 programs parsed1)) ;; ~422ms, "gfabehpdojkcimnl"
+  (quick-bench (doall (insta-parser (data))))       ;; Instaparse:   ~582ms
+  (quick-bench (doall (handwritten-parser (data)))) ;; Handwritten:  ~7ms
+  (quick-bench (doall (kern-parser (data))))        ;; Kern:         ~113ms
+  (quick-bench (doall (spec-parser (data))))        ;; clojure.spec: ~413ms
+  (= (insta-parser (data))
+     (handwritten-parser (data))
+     (kern-parser (data))
+     (spec-parser (data))) ;; true
+  (def parsed (handwritten-parser (data)))
+  (quick-bench (part-1 programs parsed)) ;; ~7ms,   "olgejankfhbmpidc"
+  (quick-bench (part-2 programs parsed)) ;; ~422ms, "gfabehpdojkcimnl"
   )
