549746f488a65c0eef587d38313e7bb93e7c817e
Michiel Borkent <michielborkent@gmail.com>
2018-03-17 14:00:30 +0100

    Fun refactor

--- src/Day02.hs
+++ src/Day02.hs
@@ -13,33 +13,7 @@ input = do
   return ls
 
 diff :: [Int] -> Int
-diff xs =
-  let highest = maximum xs
-      lowest  = minimum xs in
-    highest - lowest
+diff = (-) <$> maximum <*> minimum
 
 part1 :: IO Int
-part1 = do
-  inp <- input
-  let diffs = map diff inp in
-    return $ sum diffs
-
--- sumMatching :: [Int] -> Int
--- sumMatching (x:xs) = snd $ foldl' countSame (x,0) (xs ++ [x]) where
---   countSame (a, sum) n
---     | a == n = (n, sum + n)
---     | otherwise = (n, sum)
-
--- part1 :: IO Int
--- part1 = sumMatching <$> input
-
--- sumMatching2 :: [Int] -> Int
--- sumMatching2 xs = foldl' countSame 0 xs' where
---   xs' = zip xs (drop l $ cycle xs)
---   l = length xs `div` 2
---   countSame sum (a, b)
---     | a == b = sum + b
---     | otherwise = sum
-
--- part2 :: IO Int
--- part2 = sumMatching2 <$> input
+part1 = sum . map diff <$> input
