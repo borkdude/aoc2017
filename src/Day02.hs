5771f27e96904688e92d4292ea23bba71da10673
Michiel Borkent <michielborkent@gmail.com>
2018-03-17 15:18:06 +0100

    ffmap

--- src/Day02.hs
+++ src/Day02.hs
@@ -1,15 +1,17 @@
 module Day02 where
 
+-- apply a function to a thing inside a thing
+-- e.g. ffmap (+1) [Just 1]
+ffmap a b = (fmap . fmap) a b
+
 input :: IO [[Int]]
-input = (fmap . fmap) read . map words . lines <$>
+input = ffmap read . map words . lines <$>
   readFile "resources/day2.txt"
 
 -- it would be nicer to read in reverse: read the file, then split it
 -- into lines, then make words from a sentence and then parse each
 -- word to an Int
 -- let's define some infix stuff
-
-(.:) a = fmap . fmap $ a -- (.) . (.), three dots you know
 
 (|>) = flip (.) -- f |> g, first f, then g
 
@@ -19,7 +21,7 @@ infixr 6 |$>
 -- better?
 input2 :: IO [[Int]]
 input2 = readFile "resources/day2.txt" |$>
-  lines |> map words |> (read .:) 
+  lines |> map words |> ffmap read  
 
 diff :: [Int] -> Int
 diff = (-) <$> maximum <*> minimum
