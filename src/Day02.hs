3e8832da994049f0b11664e45cd9bb34cfdfe2ff
Michiel Borkent <michielborkent@gmail.com>
2018-03-17 15:11:35 +0100

    Infix shizzle

--- src/Day02.hs
+++ src/Day02.hs
@@ -1,20 +1,25 @@
 module Day02 where
 
-import Data.Char (digitToInt, isSpace)
-import Data.List (foldl')
-import Data.Maybe (mapMaybe)
-import Util (liftPredMaybe)
-import Debug.Trace
-
 input :: IO [[Int]]
-input = (fmap . fmap) read . map words . lines <$> readFile "resources/day2.txt"
+input = (fmap . fmap) read . map words . lines <$>
+  readFile "resources/day2.txt"
 
-(|>) = flip (.)
-(|$>) a b = fmap b a
+-- it would be nicer to read in reverse: read the file, then split it
+-- into lines, then make words from a sentence and then parse each
+-- word to an Int
+-- let's define some infix stuff
 
+(.:) a = fmap . fmap $ a -- (.) . (.), three dots you know
+
+(|>) = flip (.) -- f |> g, first f, then g
+
+infixr 6 |$>
+(|$>) a b = fmap b a -- Just 1 |$> (+1) == Just 2
+
+-- better?
 input2 :: IO [[Int]]
-input2 = readFile "resources/day2.txt" |$> (lines |> map words |> (fmap . fmap) read)
-
+input2 = readFile "resources/day2.txt" |$>
+  lines |> map words |> (read .:) 
 
 diff :: [Int] -> Int
 diff = (-) <$> maximum <*> minimum
